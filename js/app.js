d428053d934d0ca8268e974f80362077a2bf7908
zheUser <hkkb8670@gmail.com>
2024-07-31 19:02:31 +0800

    測試

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/TTT.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/TTTTES.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
